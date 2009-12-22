a3e69e91b809e03d0b4ec8e6d1d36a37d2e7441b
Anthony Brummett <brummett@gmail.com>
2009-12-21 22:57:18 -0600

    Passing the first test (simple_order.t);

--- Inventory/Item.pm
+++ Inventory/Item.pm
@@ -14,9 +14,24 @@ class Inventory::Item {
         barcode => { is => 'varchar' },
         desc    => { is => 'varchar', is_optional => 1 },
         sku     => { is => 'varchar' },
+        count   => { is_calculated => 1 },
+
+        order_item_details => { is => 'Inventory::OrderItemDetail', reverse_as => 'item', is_many => 1 },
     ],
     schema_name => 'Inventory',
     data_source => 'Inventory::DataSource::Inventory',
 };
+
+
+sub count {
+    my $self = shift;
+
+    my @details = Inventory::OrderItemDetail->get(item_id => $self);
+    my $sum = 0;
+    $sum += $_->count foreach @details;
+
+    return $sum;
+}
+
 
 1;
--- Inventory/OrderItemDetail.pm
+++ Inventory/OrderItemDetail.pm
@@ -11,7 +11,7 @@ class Inventory::OrderItemDetail {
         order_item_detail_id => { is => 'integer' },
     ],
     has => [
-        count    => { is => 'integer' },
+        count    => { is => 'integer', valid_values => [1, -1] },
         item     => { is => 'Inventory::Item', id_by => 'item_id', constraint_name => 'ORDER_ITEM_DETAIL_ITEM_ID_ITEM_ITEM_ID_FK' },
         item_id  => { is => 'integer' },
         order    => { is => 'Inventory::Order', id_by => 'order_id', constraint_name => 'ORDER_ITEM_DETAIL_ORDER_ID_ORDERS_ORDER_ID_FK' },
