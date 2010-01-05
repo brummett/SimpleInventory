c87ed62c87338a32dcda56dece43a0cde0e14284
Anthony Brummett <brummett@gmail.com>
2010-01-04 23:14:24 -0600

    I::C::Sale handles picklist orders
    it's execute() detects when you give it a pre-existing picklist order
    number and delegates over to FillPickList to do it

--- Inventory/Command/Sale.pm
+++ Inventory/Command/Sale.pm
@@ -18,4 +18,27 @@ sub _count_for_order_item_detail {
     -1;
 }
 
+
+sub execute {
+    my $self = shift;
+
+    my $order_number = $self->resolve_order_number();
+
+    my $picklist = Inventory::Order::PickList->get(order_number => $order_number);
+    if ($picklist) {
+        # Delegate to the fill pick list command...
+        my $cmd = Inventory::Command::FillPickList->create(order_number => $picklist->order_number);
+        unless ($cmd) {
+            $self->error_message("Couldn't start up a Fill Pick List Command");
+            return;
+        }
+        return $cmd->execute();
+    }
+
+    $self->order_number($order_number);
+
+    my $super = $self->super_can('_execute_body');
+    return $super->($self);
+}
+
 1;
