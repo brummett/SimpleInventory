752e51d58110416b8d5ba69fb78e79afcce805cb
Anthony Brummett <brummett@gmail.com>
2010-01-06 12:49:12 -0600

    Add the 'source' column to the default show list

--- Inventory/Command/List/Orders.pm
+++ Inventory/Command/List/Orders.pm
@@ -9,7 +9,7 @@ class Inventory::Command::List::Orders {
     is => 'Inventory::Command::List',
     has => [
         subject_class_name => { default_value => 'Inventory::Order' },
-        show => { default_value => 'order_number,order_type_name,date,item_detail_count,item_count' },
+        show => { default_value => 'order_number,order_type_name,date,item_detail_count,item_count,source' },
     ],
 };
 
